9174fb499ccc0edeadbe5ec6ebed573044fa9284
raven599 <rtj599@yeah.net>
2017-07-17 14:53:12 +0800

    0.1----已调通

--- ssm/src/main/java/com/rtj/controller/BaseController.java
+++ ssm/src/main/java/com/rtj/controller/BaseController.java
@@ -1,10 +1,23 @@
 package com.rtj.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * 基础控制器
  * @description 
  * @author RTJ
  */
+@Controller
 public class BaseController {
-
+	@RequestMapping(method=RequestMethod.POST,value="/portal.do")
+	public String dispatchRequest(HttpServletRequest req,HttpServletResponse rsp){
+		String transType=req.getHeader("transType");
+		String subTransType=req.getHeader("subTransType");
+		return "redirect:/"+transType+"/"+subTransType;
+	}
 }
